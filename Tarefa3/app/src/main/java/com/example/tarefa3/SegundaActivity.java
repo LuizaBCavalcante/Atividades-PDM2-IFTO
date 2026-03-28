a4dd125320db1136fdd99227db636aea948151d7
Luiza Cavalcante <bcavalcante.luiza@gmail.com>
2026-03-27 21:02:29 -0300

    Correção da tarefa 3 #3

--- Tarefa3/app/src/main/java/com/example/tarefa3/SegundaActivity.java
+++ Tarefa3/app/src/main/java/com/example/tarefa3/SegundaActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.example.tarefa3.databinding.ActivitySegundaBinding;
@@ -18,49 +19,29 @@ public class SegundaActivity extends AppCompatActivity {
 
     private ActivitySegundaBinding binding;
     private StudentAdapter adapter;
+    private SegundaViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         binding = DataBindingUtil.setContentView(this, R.layout.activity_segunda);
+        viewModel = new ViewModelProvider(this).get(SegundaViewModel.class);
+        configurarRecyclerView();
 
-        String json = getIntent().getStringExtra("lista_estudantes");
-        List<Student> lista = converterParaLista(json);
+        ArrayList<Student> listaEstudantes = getIntent().getParcelableArrayListExtra("alunos");
 
-        configurarRecyclerView(lista);
-
-        exibirMediaGeral(lista);
-    }
-
-    private List<Student> converterParaLista(String json) {
-        if (json != null && !json.isEmpty()) {
-            Gson gson = new Gson();
-            Type type = new TypeToken<List<Student>>() {}.getType();
-            return gson.fromJson(json, type);
+        if(listaEstudantes != null){
+            viewModel.setListaEstudantes(listaEstudantes);
+            adapter.atualizaAdapter((viewModel.getListaEstudantes().getValue()));
+            binding.txtMediaGeral.setText(viewModel.obterMediaGeral());
+        } else{
+            binding.txtMediaGeral.setText("Sem dados");
         }
-        return new ArrayList<>();
     }
-
-    private void configurarRecyclerView(List<Student> lista) {
+    private void configurarRecyclerView() {
         adapter = new StudentAdapter();
         binding.rvStatus.setLayoutManager(new LinearLayoutManager(this));
         binding.rvStatus.setAdapter(adapter);
-        adapter.atualizaAdapter(lista);
     }
 
-    private void exibirMediaGeral(List<Student> lista) {
-        if (lista == null || lista.isEmpty()) {
-            binding.txtMediaGeral.setText("Média Geral: 0.0");
-            return;
-        }
-
-        double somaMedias = 0;
-        for (Student s : lista) {
-            somaMedias += s.getMedia();
-        }
-        double mediaGeral = somaMedias / lista.size();
-
-        binding.txtMediaGeral.setText(String.format("Média Geral da Turma: %.2f", mediaGeral));
-    }
 }
